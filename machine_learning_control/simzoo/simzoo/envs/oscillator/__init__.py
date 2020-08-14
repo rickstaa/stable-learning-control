6570cdcca21e03c73a8aef5a6421821eead621fa
rickstaa <rick.staa@outlook.com>
2020-08-14 12:14:32 +0200

    :fire: Removes broken submodule

--- machine_learning_control/simzoo/simzoo/envs/oscillator/__init__.py
+++
@@ -1,5 +0,0 @@
-"""A gym environment for a synthetic oscillatory network of transcriptional regulators
-(repressilator)."""
-
-# Import modules
-from simzoo.envs.oscillator.oscillator import Oscillator
