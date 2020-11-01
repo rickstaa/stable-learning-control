b86febb548d4506d569fb624484fbcfa3adf1f23
Rick <rick.staa@outlook.com>
2020-11-01 15:19:27 +0100

    :twisted_rightwards_arrows: Test ci (#27)
    * :wrench: Updates pull CI config file
    
    * :bug: Fixes permissions bug in pull CI config
    
    * :sparkles: Updates black formatter CI rule
    
    * :art: Improves python code formatting
    
    * :bug: Fixes black CI formatting config
    
    * :bug: Fixes CI config bug
    
    * :bug: Fixes pylint CI config bug
    
    * :bug: Changes pull CI branch name
    
    * :bug: Fixes flake8 bug
    
    * :bug: Fixes flake8 CI bug
    
    * :bug: Fixes flake8 CI bug
    
    * :bug: Fixes black CI config bug
    
    * :sparkles: Updates CI config file name

--- machine_learning_control/control/algos/lac/lac copy_copy_before_lambda_debug.py
+++ machine_learning_control/control/algos/lac/lac copy_copy_before_lambda_debug.py
@@ -732,12 +732,10 @@ def lac(
             pi_optimizer, lr_lambda=lr_decay_a
         )
         q_opt_scheduler = torch.optim.lr_scheduler.LambdaLR(
-            q_optimizer,
-            lr_lambda=lr_decay_c,
+            q_optimizer, lr_lambda=lr_decay_c,
         )
         l_opt_scheduler = torch.optim.lr_scheduler.LambdaLR(
-            l_optimizer,
-            lr_lambda=lr_decay_l,
+            l_optimizer, lr_lambda=lr_decay_l,
         )
         log_alpha_opt_scheduler = torch.optim.lr_scheduler.LambdaLR(
             log_alpha_optimizer, lr_lambda=lr_decay_a
@@ -869,8 +867,7 @@ def lac(
             )
         else:
             logger.store(
-                tb_write=logger_kwargs["use_tensorboard"],
-                Alpha=alpha,
+                tb_write=logger_kwargs["use_tensorboard"], Alpha=alpha,
             )
 
         # Optimize the lagrance multiplier for the current policy
