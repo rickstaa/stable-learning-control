a03c2d0bab9a637f954ba271a925a3e92c8fc56c
rickstaa <rick.staa@outlook.com>
2020-12-04 12:59:20 +0100

    :bug: Fixes a bug that was introduced due to e564b2ea06
    Changing to using the pyproject.toml file broke the shortened submodule
    namespace. I therefore re-added the code that creates this virtual
    shortened namespace for the simzoo package to the setup.py file.

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
-#!/usr/bin/env python
-# Minimal setup.py tool required to install packages in editable mode while using
-# PEP517/518.
+"""Setup file for the 'machine_learning_control' python package.
+"""
 
-import setuptools
+from setuptools import setup, find_namespace_packages
+import re
 
-if __name__ == "__main__":
-    setuptools.setup()
+with open("README.md") as f:
+    readme = f.read()
+
+# Add extra virtual shortened package for each of namespace_pkgs
+PACKAGES = find_namespace_packages(include=["machine_learning_control*"])
+namespace_pkgs = ["simzoo"]
+exclusions = r"|".join(
+    [r"\." + item + r"\.(?=" + item + r".)" for item in namespace_pkgs]
+)
+PACKAGE_DIR = {}
+for package in PACKAGES:
+    sub_tmp = re.sub(exclusions, ".", package)
+    if sub_tmp is not package:
+        PACKAGE_DIR[sub_tmp] = package.replace(".", "/")
+PACKAGES.extend(PACKAGE_DIR.keys())
+
+# Run python setup
+setup(
+    packages=PACKAGES, package_dir=PACKAGE_DIR,
+)
