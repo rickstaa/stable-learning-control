957a5ea0d92033407f6d423b3d10c4484dc325d8
Rick Staa <rick.staa@outlook.com>
2021-03-30 13:45:49 +0200

    :white_check_mark: Adds gym environment test script

--- bayesian_learning_control/control/algos/pytorch/lac/lac.py
+++ bayesian_learning_control/control/algos/pytorch/lac/lac.py
@@ -1280,8 +1280,8 @@ if __name__ == "__main__":
     parser.add_argument(
         "--env",
         type=str,
-        # default="Oscillator-v1",
-        default="CartPoleCost-v0",  # DEBUG
+        default="Oscillator-v1",
+        # default="CartPoleCost-v0",  # DEBUG
         help="the gym env (default: Oscillator-v1)",
     )
     parser.add_argument(
