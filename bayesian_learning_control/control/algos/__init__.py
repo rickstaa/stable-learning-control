62cde45b3777dba680992a4baa3d3a5bcc44f698
Rick Staa <rick.staa@outlook.com>
2021-04-08 17:41:43 +0200

    :building_construction: Improves package architecture
    The algorithms can now also be imported directly from the algos folder.

--- bayesian_learning_control/control/algos/__init__.py
+++ bayesian_learning_control/control/algos/__init__.py
@@ -1,6 +1,9 @@
 """Contains the Pytorch and Tensorflow RL/IL algorithms.
 """
 
-# Put algorithms on namespace for easy loading in the test_policy utility
-from bayesian_learning_control.control.algos.pytorch.lac.lac import LAC
-from bayesian_learning_control.control.algos.pytorch.sac.sac import SAC
+# Put algorithms on namespace
+from bayesian_learning_control.control.algos.pytorch.lac.lac import LAC as LAC_pytorch
+from bayesian_learning_control.control.algos.pytorch.sac.sac import SAC as SAC_pytorch
+
+from bayesian_learning_control.control.algos.tf2.sac.sac import SAC as SAC_tf
+from bayesian_learning_control.control.algos.tf2.lac.lac import LAC as LAC_tf
