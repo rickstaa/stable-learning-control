e80b5780f22ce6eafc74f8c04a268959b9e83208
Rick Staa <rick.staa@outlook.com>
2021-04-11 11:19:36 +0200

    :rewind: Removes gpl algorithms from master branch
    In the previous commit I merged changes from the gpl branch into the
    master branch. This was done since the two branches converged to much
    and I needed some changes from the gpl branch. I however do not think it
    is a smart idea to add the new (unfinished) gpl algorithms to the main branch
    I will continue development on branch gpl2.

--- bayesian_learning_control/control/__init__.py
+++ bayesian_learning_control/control/__init__.py
@@ -2,24 +2,7 @@
 """
 
 from bayesian_learning_control.control.algos.pytorch.lac.lac import lac as lac_pytorch
-from bayesian_learning_control.control.algos.pytorch.lac2.lac2 import (
-    lac2 as lac2_pytorch,
-)
-from bayesian_learning_control.control.algos.pytorch.lac3.lac3 import (
-    lac3 as lac3_pytorch,
-)
-from bayesian_learning_control.control.algos.pytorch.lac4.lac4 import (
-    lac4 as lac4_pytorch,
-)
-from bayesian_learning_control.control.algos.pytorch.lac5.lac5 import (
-    lac5 as lac5_pytorch,
-)
-from bayesian_learning_control.control.algos.pytorch.lac6.lac6 import (
-    lac6 as lac6_pytorch,
-)
 from bayesian_learning_control.control.algos.pytorch.sac.sac import sac as sac_pytorch
-from bayesian_learning_control.control.algos.pytorch.sac2.sac2 import (
-    sac2 as sac2_pytorch,
 )
 
 from bayesian_learning_control.utils.import_utils import import_tf
--- bayesian_learning_control/control/algos/pytorch/sac2/__init__.py
+++
@@ -1,4 +0,0 @@
-"""A Soft Actor-Critic Agent.
-"""
-
-from bayesian_learning_control.control.algos.pytorch.sac.sac import sac
