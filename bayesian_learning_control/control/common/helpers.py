8ffa4fb7e3577a9d99a548c99e8c15b1b37e7033
rickstaa <rick.staa@outlook.com>
2021-06-12 19:12:32 +0200

    :wrench: Updates dependencies and doc

--- bayesian_learning_control/control/common/helpers.py
+++ bayesian_learning_control/control/common/helpers.py
@@ -95,18 +95,14 @@ def get_activation_function(activation_fn_name, backend="torch"):
 def discount_cumsum(x, discount):
     """Calculate the discounted cumsum.
 
-    magic from rllab for computing discounted cumulative sums of vectors.
+    .. seealso::
+        Magic from rllab for computing discounted cumulative sums of vectors.
 
-    input:
-        vector x,
-        [x0,
-         x1,
-         x2]
+    Input:
+        vector x: [x0, x1, x2]
 
-    output:
-        [x0 + discount * x1 + discount^2 * x2,
-         x1 + discount * x2,
-         x2]
+    Output:
+        [x0 + discount * x1 + discount^2 * x2, x1 + discount * x2, x2]
     """
     return scipy.signal.lfilter([1], [1, float(-discount)], x[::-1], axis=0)[::-1]
 
