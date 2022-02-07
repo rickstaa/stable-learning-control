c9ac88b33c2231f7d911a9fba6cb5ab04f9622ed
rickstaa <rick.staa@outlook.com>
2022-02-07 11:53:39 +0100

    refactor: updates README and experiment config

---
+++ TODOs.md
@@ -0,0 +1,14 @@
+# TODOs
+
+## Main
+
+- [ ] Add way to diable ros_gazebo_gym logging to file.
+- [ ] Fix environment test problems.
+- [ ] Fix save_info.json not found in load_pytorch_policy when in parent folder.
+
+
+## Docs
+
+- [ ] Sphinx python highlight goes wrong see manual policy loading.
+- [ ] Update BLC steps for when environment can not be loaded.
+- [ ] Add policy load functions to documentation.
---
+++ sandbox/test_saved_env.py
@@ -0,0 +1,34 @@
+"""Small test script to test why the saved agent that was trained on the ros_gazebo_gym
+Panda environment can not be loaded.
+"""
+
+import ros_gazebo_gym  # noqa: F401
+from bayesian_learning_control.control.utils.test_policy import (
+    load_policy_and_env,
+    run_policy,
+    load_pytorch_policy,
+    # load_tf_policy,
+)
+import gym
+
+AGENT_FOLDER = "/home/ricks/Development/bayesian-learning-control/data/staa_lac_panda_reach/staa_lac_panda_reach_s12345/torch_save"  # noqa: E501
+
+if __name__ == "__main__":
+    # STEP 1a: Try to load the policy and environment
+    # NOTE: This might fail if the environment was not
+    try:
+        _, policy = load_policy_and_env(AGENT_FOLDER)
+    except Exception:
+        # STEP: 1b: If step 1 fails recreate the environment and load the Pytorch/TF2
+        # agent
+        env = gym.make("PandaReach-v1")
+        env = gym.wrappers.FlattenObservation(
+            env
+        )  # NOTE: Done to make sure the alg works with dict observation spaces
+        policy = load_pytorch_policy(
+            AGENT_FOLDER, itr="last", env=env
+        )  # Load Pytorch agent
+        # load_tf_policy(AGENT_FOLDER, itr="last", env=env)  # Load TF2 agent
+
+    # Step 2: Try to run the policy on the environment
+    run_policy(env, policy)
