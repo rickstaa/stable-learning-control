e7c57254efb481eabe7567c7c71d35d0d21df6e2
rickstaa <rick.staa@outlook.com>
2023-06-21 11:35:33 +0200

    ci: improve versioning

---
+++ bayesian_learning_control/__init__.py
@@ -0,0 +1,6 @@
+"""Module that initializes the bayesian_learning_control package.
+"""
+
+# Make module version available.
+from .version import __version__  # noqa: F401
+from .version import __version_tuple__  # noqa: F401
