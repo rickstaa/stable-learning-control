8ee23ea36a0279081148acbd48f0a456d7b850f3
Rick Staa <rick.staa@outlook.com>
2024-03-13 21:54:41 +0100

    chore(exp): add SAC extra experiments (#426)
    This commit adds the extra experiments for the SAC algorithm in the
    cartpole, oscillator and comp oscillator environments.

--- experiments/staa_et_al_2024/run_lambda_lr_check_experiment_seed3658.bash
+++ experiments/staa_et_al_2024/run_lambda_lr_check_experiment_seed3658.bash
@@ -1,4 +1,3 @@
-# This script executes the lambda_lr_check_experiment for seed 3658 for all environments
 python -m stable_learning_control.run --exp_cfg experiments/staa_et_al_2024/cartpole/lambda_lr_check/han2020_reproduction_lac_cartpole_cost_alpha3_tune_experiment_seed3658_lambda_lr_check.yml
 python -m stable_learning_control.run --exp_cfg experiments/staa_et_al_2024/comp_oscillator/lambda_lr_check/han2020_reproduction_lac_oscillator_complicated_alpha3_tune_experiment_seed3658_lambda_lr_check.yml
 python -m stable_learning_control.run --exp_cfg experiments/staa_et_al_2024/fetch_reach/lambda_lr_check/han2020_reproduction_lac_fetch_reach_alpha3_tune_experiment_seed3658_lambda_lr_check.yml
